664628621492f322a12655f55e0ba8c76b7e9e14
Administrator <kied1594@naver.com>
2021-10-04 17:06:07 +0900

    20211004

--- src/ch06/StaticCalc.java
+++ src/ch06/StaticCalc.java
@@ -4,12 +4,8 @@ public class StaticCalc {
     // static 은 메모리에 스스로 올라간다. ( 클래스 메모리 영역 )
     // static 은 미리 메모리에 올라가있다.
     // static 은 객체화 (인스턴스(new~~))가 필요없음.
-    public static int sum(int n1, int n2)
-    {
+    public static int sum(int n1, int n2) {
         return n1 + n2;
     }
-
-    // static 메소드는 장려
-    // 1. 파라미터 값만 사용할 때 ( 멤버필드 사용 X )
-    // 2. 멤버필드를 사용하는데 그 멤버필드가 static인 경우
 }
+
